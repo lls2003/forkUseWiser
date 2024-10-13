7cc2c4ae2c38afd3b64987cd7fa87f2a420fe3cd
林令帅 <14249345+lin-lingshuai@user.noreply.gitee.com>
2024-10-13 11:12:27 +0800

    add comment

--- Solution10.java
+++ Solution10.java
@@ -28,6 +28,8 @@
  * 输入的分数个数范围是 [1,10]。
  * 最终结果的分子与分母保证是 32 位整数范围内的有效整数。
  */
+
+//做的不错，思路清晰，代码简洁
 class Solution10 {
     public String fractionAddition(String expression) {
         long x = 0, y = 1; // 分子，分母
